6467802e2989b94bb63453b9123a568199359ccb
Rafael Pereira de Paula <91758193+rafael19930509@users.noreply.github.com>
2025-09-19 19:49:42 -0300

    PrimeiroCommit
    Criação do projeto

---
+++ ChatAPI/ChatAPI/Context/DBChatContext.cs
@@ -0,0 +1,20 @@
+﻿using ChatAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ChatAPI.Context
+{
+    public class DBChatContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DBChatContext(DbContextOptions<DBChatContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=DBChatApi;Username=postgres;Password=ig+XT8sVC6llO#-%");
+            //optionsBuilder.UseNpgsql("Host=db.zpgnifgksqsiolvxerts.supabase.co;Database=postgres;Username=postgres;Password=ig+XT8sVC6llO#-%;SSL Mode=Require;Trust Server Certificate=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ ChatAPI/ChatAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using ChatAPI.Context;
+using ChatAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        public readonly DBChatContext db;
+
+        public UsuarioController(DBChatContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        public List<Usuario> Get()
+        {
+            var lista = this.db.Usuarios.ToList();
+            
+            return lista;
+        }
+    }
+}
